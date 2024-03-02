c95d5430699990f2a991abc25f2b1106962d4ebb
Saif Alaqqad <saifo1999@gmail.com>
2024-03-02 16:13:52 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using Jint;
+using Jint.Native.Function;
+
+var parser = InitJsParser();
+
+
+static Function InitJsParser()
+{
+    var jsEngine = new Engine(options =>
+    {
+        options.EnableModules(@$"{Environment.CurrentDirectory}\js");
+    });
+
+    var commentParserModule = jsEngine.Modules.Import("./comment-parser/es6/index.js");
+    return commentParserModule.Get("parse").AsFunctionInstance();
+}
